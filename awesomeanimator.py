42e54c471437c4b4eb0142cc7d65a000afbe0516
ttruijs <tigoruijs@hotmail.com>
2021-05-29 17:24:54 +0200

    Added image animator

--- awesomeanimator.py
+++ awesomeanimator.py
@@ -3,7 +3,7 @@ import multiprocessing as mp
 import matplotlib.pyplot as plt
 import time
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Tuple
 from collections import namedtuple
 
 Msg = namedtuple("Msg", ["animator_index", "plot_index", "data"])
@@ -51,12 +51,39 @@ class PlotAnimator(Animator):
     def update(self, plot_index, data):
         self.data[plot_index] = np.roll(self.data[plot_index], -1)
         self.data[plot_index][-1] = data
-        for i, (line, d) in enumerate(zip(self.lines, self.data)):
-            line.set_ydata(d)
+        self.lines[plot_index].set_ydata(self.data[plot_index])
 
-            minimum, maximum = np.min(d), np.max(d)
-            vertlen = maximum-minimum
-            self.axes[i].set_ylim(minimum-.1*abs(vertlen), maximum+.1*abs(vertlen))
+        minimum, maximum = np.min(self.data[plot_index]), np.max(self.data[plot_index])
+        vertlen = maximum-minimum
+        self.axes[plot_index].set_ylim(minimum-.1*abs(vertlen), maximum+.1*abs(vertlen))
+
+class ImageAnimator(Animator):
+
+    def __init__(self, nrows: int, ncols: int, cmaps: List[str] = None):
+        self.nrows= nrows
+        self.ncols = ncols
+        self.cmaps = cmaps
+
+    def init_animation(self):
+        self.fig, self.axes = plt.subplots(self.nrows, self.ncols)
+
+        if self.nrows != 1 and self.ncols != 1:
+            self.axes = [ax for axs in self.axes for ax in axs]
+        elif self.nrows == 1 and self.ncols == 1:
+            self.axes = [self.axes]
+
+        if self.cmaps == None:
+            for ax in self.axes:
+                ax.imshow(np.zeros((1, 1)))
+        else:
+            for ax, cmap in zip(self.axes, self.cmaps):
+                ax.imshow(np.zeros((1, 1)), cmap=cmap)
+
+    def update(self, plot_index, data):
+        if self.cmaps == None:
+            self.axes[plot_index].imshow(data)
+        else:
+            self.axes[plot_index].imshow(data, cmap=self.cmaps[plot_index])
 
 class AwesomeAnimator(mp.Process):
 
@@ -86,16 +113,20 @@ class AwesomeAnimator(mp.Process):
             while not self.q.empty():
                 msg = self.q.get()
                 self.animators[msg.animator_index].update(msg.plot_index, msg.data)
-                print()
             for animator in self.animators:
                 animator.fig.canvas.draw()
             plt.pause(.0001)
             time.sleep(self.interval)
 
 if __name__ == "__main__":
-    ani = PlotAnimator(1, 2, 1000)
-    p = AwesomeAnimator([ani], .2)
+    ani = ImageAnimator(1, 1, cmaps=['RdBu'])
+    ani2 = PlotAnimator(1, 2, 100)
+    p = AwesomeAnimator([ani, ani2], 2)
     p.start()
-    p.push(0, 0, 1)
+    p.push(0, 0, np.array([[i for i in range(32)] for j in range(32)]))
+    while True:
+        p.push(1, 0, np.random.randint(0, 2))
+        p.push(1, 1, np.random.randint(0, 100)*.01)
+        time.sleep(1)
     p.join()
 
